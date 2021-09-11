dd17117bb6daeeb4d779cbaf91200501d3ceb512
FederalBureau911 <75162989+FederalBureau911@users.noreply.github.com>
2021-09-11 18:29:43 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var mk, mkimg
 var pistol, pistolimg
 var bcgwar, bcgwarimg
 var ground
+var obstacle
 
 function preload(){
 
@@ -18,13 +19,35 @@ function preload(){
 
 function setup() {
   createCanvas(1000,1000);
-  //createSprite(400, 200, 50, 50);
-  lmck = createSprite(200,500,20,20)
-  lmck.addImage(lmckimg)
-  lmck.setCollider("rectangle", -20, -20, 100, 210)
-  lmck.debug = true
-  ground = createSprite(500,605, 1000, 20)
+
   
+
+  lmck = createSprite(200,500,20,20)
+  lmck.addImage(lmckimg);
+  lmck.setCollider("rectangle", 5, -20, 30, 250)
+  //lmck.debug = true
+
+  obstacleGroup=new Group();
+
+  obstacle1 = createSprite(440,580, 60, 25)
+  obstacle2 = createSprite(485,550, 30, 30)
+  obstacle3 = createSprite(530,520, 60, 30);
+  obstacle4 = createSprite(560,550, 60, 25)
+  obstacle5 = createSprite(590,580, 130, 25)
+  obstacle6 = createSprite(200,580, 400, 25)
+  obstacle7 = createSprite(850,580, 400, 25)
+
+  obstacleGroup.add(obstacle1);
+  obstacleGroup.add(obstacle2);
+  obstacleGroup.add(obstacle3);
+  obstacleGroup.add(obstacle4);
+  obstacleGroup.add(obstacle5);
+  obstacleGroup.add(obstacle6);
+  obstacleGroup.add(obstacle7);
+  
+  
+ 
+ 
 }
 
 
@@ -32,22 +55,32 @@ function setup() {
 function draw() {
   background(bcgwarimg);
   
-
-  lmck.velocityY+=1.6
-  
   if(keyDown(LEFT_ARROW)){
     fill("BLUE")
     textSize(30)
     text("I should go ahead first", lmck.x-150, 370)
   }
-  if(keyDown(RIGHT_ARROW)){
-    lmck.x+=4
-    
-  }
 
-  if(keyDown(UP_ARROW)&&lmck.collide(ground)){
+  if(keyDown(RIGHT_ARROW)){
+    lmck.x+=4  
+  }
+ 
+  /*
+  obstacle1.visible=false
+  obstacle2.visible=false
+  obstacle3.visible=false
+  obstacle4.visible=false
+  obstacle5.visible=false
+  obstacle6.visible=false
+*/
+  if(keyDown(UP_ARROW)&& lmck.collide(obstacleGroup)){
     lmck.velocityY=-10
   }
-  lmck.collide(ground) 
+
+  lmck.velocityY+=1.6
+  
+  lmck.collide(obstacleGroup)
+
   drawSprites();
+
 }
